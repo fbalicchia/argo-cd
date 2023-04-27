5192cbecdfcc5e447f835ee42d2577042810a398
fbalicchia <fbalicchia@cuebiq.com>
2023-04-27 13:37:44 +0200

    remove premature return

--- resource_customizations/argoproj.io/Application/health.lua
+++ resource_customizations/argoproj.io/Application/health.lua
@@ -6,16 +6,13 @@ if obj.status ~= nil then
     hs.status = obj.status.health.status
     if obj.status.health.message ~= nil then
       hs.message = obj.status.health.message
-      return hs
     end
     if hs.status == "Healthy" then
       for i, group in ipairs(obj.status.resources) do
         if group.health ~= nil and group.health.status ~= nil and group.health.status ~= "Healthy" then
           hs.status = group.health.status
           hs.message = "Leaf Applications " .. group.health.status
-          return hs
         end
-
       end
     end
   end
